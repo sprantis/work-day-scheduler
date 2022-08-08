88043f82f132ad2750b2517f34c0e2bdedd69176
Sarah Prantis <sprantis@gmail.com>
2022-08-07 20:37:02 -0400

    added JS functions to populate time rows and save notes

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,8 @@
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
+let dateTime = $("#currentDay").text(
+  moment().format("MMMM Do YYYY, h:mm:ss a")
+);
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// Defined Variables
-// // array of time between 9am-5pm
+// Array of time between 9am-5pm
 let timeArray = [
   "9:00 AM",
   "10:00 AM",
@@ -29,28 +14,35 @@ let timeArray = [
   "4:00 PM",
   "5:00 PM",
 ];
+
 let table = document.querySelector("#table");
 
-// // define a function then execute the function after its definition
+// Code below created with the assistance from tutor Sangeetha Kaliaperumal
+// Generate timeBlock for each business hour
 function timeBlock() {
   let j = 9;
   for (i = 0; i < timeArray.length; i++) {
     let tableRow = document.createElement("tr");
-    let tableCell1 = document.createElement("td");
 
+    let tableCell1 = document.createElement("td");
     let hourCell = document.createElement("p");
-    hourCell.setAttribute("class", "hour");
 
     let tableCell2 = document.createElement("td");
-    tableCell2.setAttribute("class", "box");
     let textArea = document.createElement("textarea");
+
+    let tableCell3 = document.createElement("td");
+    let btn = document.createElement("div");
+
+    tableCell1.setAttribute("class", "tableCell1 col-2");
+    hourCell.setAttribute("class", "hour");
+
+    tableCell2.setAttribute("class", "col-9 box tableCell2");
     textArea.setAttribute("class", "description");
     textArea.setAttribute("id", j);
     j++;
     tableCell2.appendChild(textArea);
 
-    let tableCell3 = document.createElement("td");
-    let btn = document.createElement("button");
+    tableCell3.setAttribute("class", "tableCell3 col-1");
     btn.setAttribute("class", "saveBtn");
     btn.textContent = "save";
     btn.addEventListener("click", saveBtn);
@@ -63,15 +55,46 @@ function timeBlock() {
 
     tableRow.append(tableCell1, tableCell2, tableCell3);
     table.append(tableRow);
-    // console.log(tableRow);
-  };
-//   console.log(timeArray);
+  }
+
+  // for loop to grab saved data from local storage set it as the value for the textarea element
+  for (let i = 9; i < 17; i++) {
+    let locstorage = localStorage.getItem(i);
+    let element = document.getElementById(i);
+    element.value = locstorage;
+  }
 }
+
 timeBlock();
-// For every hour, display a row that contains time, input field, and save button from hours 9am-5pm
-function saveBtn(event){
-  console.log(event.target);
-//   let input = event.target.closest("textarea").value;
-//   let hour =  event.target.closest("textarea").getAttribute("id")
-  console.log(input, hour);
+
+function saveBtn() {
+  let input = $(this).parent().siblings(".box").children(".description").val(); //event.target.closest("textarea").value;
+  let hour = $(this)
+    .parent()
+    .siblings(".box")
+    .children(".description")
+    .attr("id"); //event.target.parentNode..getAttribute("id")
+  localStorage.setItem(hour, input);
 }
+
+// Apply color code to textarea when compared to current time
+function tbColor(element) {
+  let hourValue = parseInt(element.attr("id"));
+  const currentTime = parseInt(moment().format("H"));
+  if (hourValue < currentTime) {
+    element.addClass("past");
+  } else if (hourValue > currentTime) {
+    element.addClass("future");
+  } else {
+    element.addClass("present");
+  }
+}
+
+// Run a check for each time block, particularly the textarea element in each block
+function checkTime() {
+  $("textarea").each(function () {
+    tbColor($(this));
+  });
+}
+
+checkTime();
